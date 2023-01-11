09cdcded97e0ac93e678ab165cee0f929ac7faf2
Raul Arias Perez <aprariasperez@gmail.com>
2023-01-11 13:36:04 +0100

    Ejercicio T2-a

--- Tarea02-a.php
+++ Tarea02-a.php
@@ -10,23 +10,33 @@
     
     if (!empty($_GET['recipe']))
     {
-        $sql = "SELECT (SELECT nombre FROM ingrediente WHERE codigo = cod_ingrediente), cantidad, medida FROM `receta_ingrediente` WHERE cod_receta=?";
+        $sql1 = "SELECT nombre FROM receta WHERE codigo=?";
+        $sql2 = "SELECT (SELECT nombre FROM ingrediente WHERE codigo = cod_ingrediente) AS nombre, cantidad, medida FROM `receta_ingrediente` WHERE cod_receta=?";
         
-        $prepsql = $db->prepare($sql);
-        $prepsql->execute(array($_GET['recipe']));
+        $prepsql1 = $db->prepare($sql1);
+        $prepsql1->execute(array($_GET['recipe']));
+        $prepsql2 = $db->prepare($sql2);
+        $prepsql2->execute(array($_GET['recipe']));
         
-        $res_arr = [];
-        while ($aux = $prepsql->fetch(pdo::FETCH_ASSOC))
-            array_push ($res_arr, $aux);
         
-        $str = "";
+        $str = "<h1>" . $prepsql1->fetch(PDO::FETCH_ASSOC)['nombre'] . "</h1>";
+        $str .= "<ul>";
         
+        while ($aux = $prepsql2->fetch(PDO::FETCH_ASSOC))
+            $str .= "<li>" . $aux['nombre'] . ": " . $aux['cantidad'] . " " . $aux['medida'] . "</li>";
+        $str .= "</ul>";
+        
+        unset($prepsql1);
+        unset($prepsql2);
+        unset($db);
+        
+        $str .= "<a href='" . htmlspecialchars($_SERVER['PHP_SELF']) . "'>&lt&ltVolver</a>";
     }
     else
     {
-        $columns = ['nombre', 'dificultad', 'tiempo', 'chef'];
+        $columns = ['nombre', 'dificultad', 'tiempo', 'chef', 'codigo'];
 
-        $sql = "SELECT nombre, dificultad, tiempo, (SELECT nombreartistico FROM chef WHERE codigo = cod_chef) AS chef FROM receta;";
+        $sql = "SELECT nombre, dificultad, tiempo, (SELECT nombreartistico FROM chef WHERE codigo = cod_chef) AS chef, codigo FROM receta;";
 
         $result = $db->prepare($sql);
         $result->execute();
@@ -34,20 +44,15 @@
         foreach ($columns as $column)
             $result->bindColumn($column, $$column);
 
-        $res_arr = [];
-
+        $str = "<table><tr><th>Nombre</th><th>Dificultad</th><th>Tiempo</th><th>Chef</th></tr>";
         while ($result->fetch(PDO::FETCH_BOUND))
-            array_push($res_arr, array($nombre, $dificultad, $tiempo, $chef));
+            $str .= "<tr><td><a href='" . "?recipe=" . $codigo . "'>" . $nombre . "</a></td><td>" . $dificultad . "</td><td>" . $tiempo . "</td><td>" . $chef . "</td></tr>";
+        $str .= "</table>";
 
         unset($result);
         unset($db);
 
-        $str = "<table><tr><th>Nombre</th><th>Dificultad</th><th>Tiempo</th><th>Chef</th></tr>";
 
-        foreach ($res_arr as $fila)
-            $str .= "<tr><td>" . $fila[0] . "</td><td>" . $fila[1] . "</td><td>" . $fila[2] . "</td><td>" . $fila[3] . "</td></tr>";
-
-        $str .= "</table>";
     }
     
 ?>
